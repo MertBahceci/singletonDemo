2c132637765f115a24c1b293923d7dd932e63b89
MertBahceci <mert.bahceci@denk-it.com>
2023-02-05 02:26:26 +0300

    Code lines fixed

--- Singleton Pattern Demo App/src/com/phoenix/constants/StaffConstants.java
+++ Singleton Pattern Demo App/src/com/phoenix/constants/StaffConstants.java
@@ -13,10 +13,9 @@ import java.math.BigDecimal;
 public class StaffConstants {
 
     public static final BigDecimal baseStaffSalary = BigDecimal.valueOf(10);
+    
     public static final BigDecimal officerConstant = BigDecimal.valueOf(3.6);
-
     public static final BigDecimal workerConstant = BigDecimal.valueOf(2.1);
-
     public static final BigDecimal medicalStaffConstant = BigDecimal.valueOf(5.2);
 
 }
